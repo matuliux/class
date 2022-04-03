6dffed692e8741c7489c218e0859fc6d43f74113
matuliux <100164021+matuliux@users.noreply.github.com>
2022-04-03 23:52:53 +0300

    Add files via upload

---
+++ V0.5_deque/bibliotekos.h
@@ -0,0 +1,32 @@
+
+#ifndef BIBLIOTEKOS_H
+#define BIBLIOTEKOS_H   #pragma once
+
+#include <deque>
+#include <stdio.h>
+#include <stdlib.h>
+#include <map>
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <vector>
+#include <random>
+#include <algorithm>
+#include <chrono>
+#include <fstream>
+#include <sstream>
+using std::cout;
+using std::cin;
+using std::string;
+using std::deque;
+using std::endl;
+using std::vector;
+using std::setw;
+using std::left;
+using std::right;
+using std::mt19937;
+using hrClock = std::chrono::high_resolution_clock;
+typedef std::uniform_int_distribution<int> int_distribution;
+
+
+#endif
