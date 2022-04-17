5ff483eb108a71d69f907d5ea50a2e1cc8497a97
matuliux <100164021+matuliux@users.noreply.github.com>
2022-04-17 22:03:50 +0300

    Add files via upload

---
+++ V1.0_1_strategija_list/bibliotekos.h
@@ -0,0 +1,29 @@
+#ifndef BIBLIOTEKOS_H
+#define BIBLIOTEKOS_H   
+
+#include <list>
+#include <stdio.h>
+#include <stdlib.h>
+#include <map>
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <random>
+#include <algorithm>
+#include <chrono>
+#include <fstream>
+#include <sstream>
+using std::cout;
+using std::cin;
+using std::string;
+using std::endl;
+using std::list;
+using std::setw;
+using std::left;
+using std::right;
+using std::mt19937;
+using hrClock = std::chrono::high_resolution_clock;
+typedef std::uniform_int_distribution<int> int_distribution;
+
+
+#endif
