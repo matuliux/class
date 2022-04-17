049bf4786353ed061ec145d3323a9f7d0c64ae1b
matuliux <100164021+matuliux@users.noreply.github.com>
2022-04-17 22:05:41 +0300

    Add files via upload

---
+++ V1.0_2_strategija_vector/header.h
@@ -0,0 +1,34 @@
+
+#ifndef HEADER_H
+#define HEADER_H
+#include "bibliotekos.h"
+
+struct Studentas
+{
+    string vardas = "";
+    string pavarde = "";
+    vector <double>nd;
+    int egz = 0;
+    int sum = 0;
+    double rez = 0;
+    double med = 0;
+    int grupe = 0; // Jeigu grupe 1 - protingas 0 - vargsiukas
+};
+
+void ar_failas(int& f);
+void failas();
+void ranka();
+void tikr_random(string& ar_random);
+void ivestis(Studentas& temp);
+void isvestis(Studentas& temp);
+void vidurkis(Studentas& temp);
+void mediana(Studentas& temp);
+bool compare_name(const Studentas& a, const Studentas& b);
+bool compare_last(const Studentas& a, const Studentas& b);
+void file_isvedimas(vector<Studentas> protingi, vector<Studentas> vargsiukai, int sk, double& isvedimas1, double& isvedimas2);
+void vertinimas(vector<Studentas>& protingi, vector<Studentas>& vargsiukai, double& pazangumas);
+void sort_mok(vector<Studentas>& list);
+void laikas(double sukurimas, double ivedimas, double rusiavimas, double isvedimas1, double isvedimas2, int sk);
+void generavimas_skaiciai(string& pav);
+bool Maziau(const Studentas& a);
+#endif
