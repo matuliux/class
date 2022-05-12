b7140d9d587cc5d32c1dba856b4e2a2211faf8ff
matuliux <100164021+matuliux@users.noreply.github.com>
2022-05-12 22:27:04 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,20 @@
+#include "header.h"
+#include "bibliotekos.h"
+
+int main()
+{
+
+    int f;
+    ar_failas(f);
+
+    if (f == 1) {
+        failas();
+    }
+
+    else {
+        ranka();
+    }
+
+
+    return 0;
+}
